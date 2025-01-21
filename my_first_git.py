0266ecbfc71f71356a6ffbc6b7b29161ef772abf
Chris-C-at-Rectory <aokun.chen@rectorywolves.org>
2025-01-21 04:31:37 -1000

    test karp

---
+++ my_first_git.py
@@ -0,0 +1,9 @@
+print("hey")
+
+def coder(m,n)
+d=[]
+a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+ask=input("Input a word you want to cypher:")
+for i in range(len(a)):
+	if x<0:
+		x+
